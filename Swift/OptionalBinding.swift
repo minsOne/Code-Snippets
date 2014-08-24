ce2cd77ba0a56f2ea49188e25320724ca159b974
minsOne <jungminahn.minsone@gmail.com>
2014-08-24 21:38:42 +0900

    Optional binding Code Snippet

---
+++ Swift/OptionalBinding.swift
@@ -0,0 +1,10 @@
+/*
+ Optional Binding in statement
+ Platform : iOS / OSX
+ Language : Swift
+ Completion Scope : Function or Method
+ */
+
+if let <#constantName#> = <#someOptional#> {
+    <#statements#>
+}
