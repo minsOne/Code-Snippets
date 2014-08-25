e220c15baa39e82ad83ed8617d50ad9e154be38f
minsOne <jungminahn.minsone@gmail.com>
2014-08-26 01:29:03 +0900

    add Code that strong and weak reference

---
+++ Objective-C/StrongWeakRef.m
@@ -0,0 +1,12 @@
+/*
+ Declare strong and weak reference to self
+ Platform : iOS / OSX
+ Language : Objective-C
+ Completion Scope : Code Expression
+ */
+
+#pragma mark - Strong reference
+__strong typeof(<#weakSelf#>)strongSelf = <#weakSelf#>;
+
+#pragma mark - Weak reference
+__weak typeof(<#strongSelf#>)weakSelf = <#strongSelf#>;
