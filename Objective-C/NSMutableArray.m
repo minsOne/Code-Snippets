a570d5fc6ab62afd188a914ad14acd91cbcbaee0
min <min@smartstudy.co.kr>
2015-07-01 16:27:44 +0900

    add snippet

---
+++ Objective-C/NSMutableArray.m
@@ -0,0 +1,11 @@
+/*
+ NSMutable Initialized Short Code
+ Platform : iOS / OSX
+ Language : Objective-C
+ Completion Scope : Code Expression
+ */
+
+#pragma mark - Code Expression
+
+NSMutableArray *array = [@[] mutableCopy];
+NSMutableArray *array = [NSMutableArray array];
