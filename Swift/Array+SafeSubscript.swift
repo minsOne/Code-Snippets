bc7f272b620efea87995475a407585c4acff942d
min <min@smartstudy.co.kr>
2015-10-11 19:18:53 +0900

    add snippet

---
+++ Swift/Array+SafeSubscript.swift
@@ -0,0 +1,18 @@
+/*
+Array extension
+Platform : iOS / OSX
+Language : Swift
+Required: iOS 9.0 and later
+*/
+
+extension Array {
+    subscript (safe index: Int) -> Element? {
+        return indices ~= index ? self[index] : nil
+    }
+}
+
+// MARK: Use
+
+let list = [1,2,3]
+list[safe: 4] // nil
+list[safe: 2] // 3
