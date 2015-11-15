dcf2d1d394d7a34ca700a62ee5cc3c37d4120a57
min <min@smartstudy.co.kr>
2015-11-15 13:50:10 +0900

    add codesnippet

---
+++ Swift/CollectionType+filter.swift
@@ -0,0 +1,28 @@
+/*
+Collection extension - seperate passed list and failed list
+Platform : iOS / OSX
+Language : Swift
+Required: iOS 9.0 and later
+*/
+
+extension CollectionType {
+    public func filter(@noescape includeElement: (Self.Generator.Element) throws -> Bool) rethrows -> (passed: [Generator.Element], failed: [Generator.Element]) {
+        var passed: [Generator.Element] = []
+        var failed: [Generator.Element] = []
+        try forEach {
+            if try includeElement($0) {
+                passed += [$0]
+            } else {
+                failed += [$0]
+            }
+        }
+        return (passed, failed)
+    }
+}
+
+
+// MARK: Use
+
+let list = [1,2,3]
+list[safe: 4] // nil
+list[safe: 2] // 3
