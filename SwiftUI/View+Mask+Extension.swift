2f15dab8fb38bd37d7435b4624fa46a0296bb660
damon.ahn <damon.ahn@kakaobank.com>
2020-02-13 11:24:58 +0900

    add gist

---
+++ SwiftUI/View+Mask+Extension.swift
@@ -0,0 +1,15 @@
+/// reference: dotSwift 2020 - Tobias Due Munk - Prototyping Custom UI in SwiftUI
+
+import SwiftUI
+
+extension View {
+	/// 해당 View에 masking을 합니다.
+	///
+	///     Text("Hello world").maskContent(using: Color.blue)
+	///     // or
+	///     Color.blue.mask(Text("Hello world"))
+    ///
+	func maskContent<T: View>(using: T) -> some View {
+		using.mask(self)
+	}
+}
