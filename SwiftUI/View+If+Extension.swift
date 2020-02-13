91b58c08256a4d0e984d848fe9bdec8cb6353b6d
damon.ahn <damon.ahn@kakaobank.com>
2020-02-13 10:48:06 +0900

    add file

---
+++ SwiftUI/View+If+Extension.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+extension View {
+    /// 조건 여부에 따라 transform 을 호출하여 View에 반영
+    ///
+    ///     var colorize: Bool = true
+    ///     Text("Hello")
+    ///       .if(colorize) { $0.background(Color.red) }
+    ///
+    /// - Parameters:
+    ///   - conditional: transform 호출 여부를 결정
+    ///   - transform: 매핑 closure. View를 transform 함.
+    ///
+    /// - Returns: 기존 View를 반환하거나 transformed View를 반환
+    func `if`<T: View>(_ conditional: Bool, transform: (Self) -> T) -> some View {
+        Group {
+            if conditional {
+                transform(self)
+            } else {
+                self
+            }
+        }
+    }
+}
